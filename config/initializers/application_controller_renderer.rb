9bceda6bd0c2adb2b6164d935cc63ec177b31d7d
usskr <rei.takagi731@gmail.com>
2021-08-09 11:06:53 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b8c2419b2ef089647e1befd795f69f575a41992396fee2549599d6e8ea32b17a85a250239972816f8c7af94c519f3e001dcf0fb5b470216182b33303f5a72ba6'
